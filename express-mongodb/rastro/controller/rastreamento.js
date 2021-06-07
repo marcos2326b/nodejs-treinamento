01930359d2fce4818b10c290d297bc4edd0527a3
Marcos Vinicius <marcos2326t@gmail.com>
2021-06-07 18:04:02 -0300

    API GET

--- express-mongodb/rastro/controller/rastreamento.js
+++ express-mongodb/rastro/controller/rastreamento.js
@@ -62,14 +62,55 @@ module.exports = app => {
                 response.status(500).send(`Erro ao conectar no banco MongoDB: ${erro}`);
             });
          
-        }
+        },
+        buscarPorCodigoRastreador(request, response){
+            console.log('Rota GET/rastramento/:codigoRastreador? chamada...')
+            console.log(`request.params:${request.params}`);
+            console.log(request.params)
+
+            if(request.params.codigoRastreador == "" || request.params.codigoRastreador == null ){
+                response.status(400).send('Parametro codigoRastreador invalido');
+            } else {
+                
+                mongoose.connect(
+                    'mongodb://localhost:27017/rastro-dev',
+                    {
+                        useNewUrlParser: true,
+                        useUnifiedTopology: true,
+                        useCreateIndex: true
+                    }    
+                )
+
+            
+
+                .then(() => {
+                    const Rastreamento = app.model.rastreamento;
+
+                    Rastreamento.find( { codigoRastreador: request.params.codigoRastreador } )
+                    .then((listaRastreamentos) => {
+                        console.log(listaRastreamentos);
+                        mongoose.disconnect();
+                        response.status(200).send(listaRastreamentos);
+                    })
+
+                    .catch((erro) => {
+                        console.log(`Erro ao realizar a consulta de rastreamentos: ${erro}`);
+                        console.log(erro);
+                        mongoose.disconnect();
+                        response.status(500).send(`Erro ao realizar a consulta de rastreamentos: ${erro}`);
+                    });
+    
+                })
+
+    
+                .catch((erro) => {
+                    console.log(`Erro ao conectar no banco MongoDB: ${erro}`);
+                    console.log(erro);
+                    response.status(500).send(`Erro ao conectar no banco MongoDB: ${erro}`);
+                });
+            }   
+        }      
 
     }
     return Rastreamentocontroller;
- 
 }
-
-
-
-
-
--- express-mongodb/rastro/routes/rastreamento.js
+++ express-mongodb/rastro/routes/rastreamento.js
@@ -1,5 +1,7 @@
 module.exports = (app) => {
 
     app.post('/rastreamento', app.controller.rastreamento.cadastrar);
-    
+
+
+    app.get('/rastreamento/:codigoRastreador', app.controller.rastreamento.buscarPorCodigoRastreador);
 }
